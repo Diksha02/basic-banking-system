73498bfb122114a942d351a285ce80c70a9223ab
Diksha Nigam <nigamdiksha02@gmail.com>
2021-07-02 23:18:29 +0530

    feat: add header, footer and home page

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {});
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    aboutContent: aboutContent,
+    contactContent: contactContent,
+  });
 });
 
 app.listen(3000, () => {
